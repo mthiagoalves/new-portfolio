ea39a043f92a28c88d52e20d4fc5173f245c8046
mthiagoalves <m.thiago.alves@live.com>
2023-06-20 16:00:48 +0100

    feat(user): create dto & entity, and validate all

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,24 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger"
+import { IsString } from "class-validator"
+
+export class CreateUserDto {
+
+  @IsString()
+  @ApiProperty({
+    description: 'Name of user',
+    example: 'Thiago Alves'
+  })
+  name: string
+
+  @ApiProperty({
+    description: 'User nickname',
+    example: 'm.thiago.alves'
+
+  })
+  nickname: string
+
+  
+  password: string
+
+
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,8 @@
-export class User {}
+export class User {
+  id?: string
+  name: string
+  nickname: string
+  password: string
+  createdAt?: Date
+  updateAt?: Date
+}
