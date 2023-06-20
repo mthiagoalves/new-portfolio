f7f8bc62c6fcd757315441962e42906cc9d500f9
mthiagoalves <m.thiago.alves@live.com>
2023-06-20 14:54:32 +0100

    feat(user): generate resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ProjectModule } from './projects/project.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { TechnologyModule } from './technology/technology.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ProjectModule, PrismaModule, TechnologyModule],
+  imports: [ProjectModule, PrismaModule, TechnologyModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
