1b9ffd69291f1684ba9b420e993c98bb47091413
mthiagoalves <m.thiago.alves@live.com>
2023-06-22 18:22:15 +0100

    chore(main): add process .env.port

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,6 @@ async function bootstrap() {
 const document = SwaggerModule.createDocument(app, config);
 SwaggerModule.setup('api', app, document);
 
-  await app.listen(3454);
+  await app.listen(process.env.PORT || 3454);
 }
 bootstrap();
