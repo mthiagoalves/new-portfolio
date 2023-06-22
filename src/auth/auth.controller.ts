8f95a58fc1b338a7402a2781eb431a7197164ca4
mthiagoalves <m.thiago.alves@live.com>
2023-06-22 17:58:11 +0100

    feat(auth): implement logged user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,6 +4,8 @@ import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { LoginDto } from './dto/login.dto';
 import { LoginRespondeDto } from './dto/login-response.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { LoggedUser } from './legged-user.decorator';
+import { User } from 'src/user/entities/user.entity';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -25,7 +27,7 @@ export class AuthController {
     summary: 'Return user auth'
   })
   @ApiBearerAuth()
-  profile() {
-    return { message: 'Login Sucess'}
+  profile(@LoggedUser() user: User) {
+    return { user };
   }
 }
